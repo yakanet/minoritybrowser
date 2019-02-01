7b5213d91f369c40f75d39198007e234e820dabc
Mathieu Broutin <mbroutin@sedona.fr>
2019-02-01 10:58:54 +0100

    feat: use normalized angle

--- fingertip2/src/circle-menu.sprite.ts
+++ fingertip2/src/circle-menu.sprite.ts
@@ -5,11 +5,27 @@ import {p, Point, Point3, radToDeg} from './geo-util';
 const {PI} = Math;
 
 export class CircleMenuSprite extends SpriteModel {
+    /**
+     * Captured coordinate
+     */
     private captures: tracking.TrackRect[];
+
     private captureType: 'SINGLE' | 'TREE_POINT' = null;
+    /**
+     * That the coordinate of the circle passing through 3 captured points
+     */
     private circle: { radius: number; center: Point };
+    /**
+     * That the point in the middle of the 3 captured points
+     */
     private middlePoint: Point;
+    /**
+     * Angle between the middlePoint and the normalPoint inside the circle
+     */
     private openAngle: number;
+    /**
+     * Easier for calculation, use the point (1, 0) on the circle
+     */
     private normalPoint: Point;
 
     updateCapture(capture: tracking.TrackRect[]) {
@@ -36,6 +52,8 @@ export class CircleMenuSprite extends SpriteModel {
             this.captureType = 'TREE_POINT';
             const [x1, x2, x3] = [0, 1, 2].map(i => this.captures[i].x + this.captures[i].width / 2);
             const [y1, y2, y3] = [0, 1, 2].map(i => this.captures[i].y + this.captures[i].height / 2);
+
+            // Calculate circle, points and angle in order to draw
             const points = new Point3(p(x1, y1), p(x2, y2), p(x3, y3));
             this.circle = points.findCircle();
             this.middlePoint = points.findMiddlePoint();
@@ -74,13 +92,32 @@ export class CircleMenuSprite extends SpriteModel {
             strokeStyle: `green`
         });
 
+        drawLine(context, circle.center.x, circle.center.y, circle.center.x, circle.center.y - circle.radius, {
+            lineWidth,
+            strokeStyle: `green`
+        });
+
         drawArc(context, circle.center.x, circle.center.y, 50, 0, angle, angle < 0, {
             lineWidth,
             strokeStyle: `white`
         });
 
-        drawText(context, circle.center.x + 10, circle.center.y - 10, Math.ceil(radToDeg(angle)) + ' °', {
+        drawText(context, circle.center.x + 10, circle.center.y - 10, Math.ceil(radToDeg(this.normalizedAngle)) + ' °', {
             fillStyle: 'red'
         });
+    }
+
+    /**
+     * Return the angle with the middle point and the (0, 1) point on the circle
+     * @return angle in radian [-PI, PI]
+     */
+    get normalizedAngle() {
+        if (this.captureType === 'SINGLE') {
+            return 0;
+        }
+        if (this.openAngle > PI / 2) {
+            return this.openAngle - 2 * PI + PI / 2;
+        }
+        return this.openAngle + PI / 2;
     }
 }
